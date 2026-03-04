4ed64920deb4314594086951f01d6c01378dca26
beenazzie <beenazzie@gmail.com>
2026-03-04 23:09:45 +0200

    Final fixes for GitHub Pages path issues and dynamic Google Maps loading

--- components/icons/LogoIcon.tsx
+++ components/icons/LogoIcon.tsx
@@ -2,5 +2,5 @@
 import React from 'react';
 
 export const LogoIcon: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ className, ...props }) => (
-  <img src="/logo.jpg" alt="Queue-Marshal Logo" className={`rounded-full ${className}`} {...props} />
+  <img src="logo.jpg" alt="Queue-Marshal Logo" className={`rounded-full ${className}`} {...props} />
 );
